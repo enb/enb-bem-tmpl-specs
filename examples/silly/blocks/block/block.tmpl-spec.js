11f3c70ae7c3c694cd83e19fed962ac4c12db1b5
andrewblond <andrewblond@yandex.com>
2014-06-26 18:38:54 +0400

    Updated examples

--- examples/silly/blocks/block/block.tmpl-spec.js
+++
@@ -1,5 +0,0 @@
-describe('block', function() {
-    it('should be equal to four', function() {
-        (2*2).should.to.equal(4);
-    });
-});
---
+++ examples/silly/blocks/image/image.tmpl-specs/10-simple.bemjson.js
@@ -0,0 +1,6 @@
+({
+    block : 'image',
+    url : 'download.svg',
+    alt : 'Download',
+    title : 'Download image'
+})
