0d18fa97cd842f513568ee24dd52752bf2c1f6d8
andrewblond <andrewblond@yandex.com>
2014-08-01 22:25:23 +0400

    Added `engines` option

--- examples/silly/.enb/make.js
+++ examples/silly/.enb/make.js
@@ -8,7 +8,27 @@ module.exports = function (config) {
     tmplSpecs.configure({
         destPath: 'tmpl-specs',
         levels: getLevels(config),
-        sourceLevels: getSourceLevels(config)
+        sourceLevels: getSourceLevels(config),
+        engines: {
+            bh: {
+                tech: 'enb-bh/techs/bh-server',
+                options: {
+                    bhFile: '../../node_modules/bh/lib/bh.js',
+                    jsAttrName: 'data-bem',
+                    jsAttrScheme: 'json'
+                }
+            },
+            bemhtmlDev: {
+                tech: 'enb-bemxjst/techs/bemhtml-old',
+                exportName: 'BEMHTML',
+                options: { devMode: true }
+            },
+            bemhtmlProd: {
+                tech: 'enb-bemxjst/techs/bemhtml-old',
+                exportName: 'BEMHTML',
+                options: { devMode: false }
+            }
+        }
     });
 };
 
